bfe82f48be35ca84a3e383b9b2a4ae20fd224768
khouloud-belwaer <38992754+khouloud-belwaer@users.noreply.github.com>
2018-08-27 13:51:13 +0200

    Update basicspec.js

--- test/basicspec.js
+++ test/basicspec.js
@@ -2,6 +2,7 @@ describe("Simple Test:", function()
 {
     it("a is infact 'hello world!' and b to be not null", function () {
         var a = "hello world!";
+        var expect = chai.expect;
         expect(a).to.be("hello world");
     });
 });
